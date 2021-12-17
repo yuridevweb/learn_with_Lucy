3c26be3a987cd12dda22599f7c2cfb19af571d36
yuridevweb <yuridevweb@gmail.com>
2021-12-17 11:27:33 +0000

    ready to deploy

--- users/urls.py
+++ users/urls.py
@@ -13,5 +13,17 @@ urlpatterns = [
          auth_views.PasswordResetView.as_view(
              template_name='users/password_reset.html'),
          name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/manage.py',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 ]
