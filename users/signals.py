984c0296d55d1adfbac6f5487f983dcce25bf261
yuridevweb <yuridevweb@gmail.com>
2021-12-18 16:40:58 +0000

    attempt No2

--- users/signals.py
+++ users/signals.py
@@ -8,11 +8,11 @@ from main.models import Pet, Dwelling, Avatar
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance,
-                               profile_pet=Pet.objects.get(name="pet_0"),
-                               profile_avatar=Avatar.objects.get(
-                                   name="avatar_0"),
-                               profile_dwelling=Dwelling.objects.get(name="dwelling_0"))
+        Profile.objects.create(user=instance)
+        # profile_pet=Pet.objects.get(name="pet_0"),
+        # profile_avatar=Avatar.objects.get(
+        # name="avatar_0"),
+        # profile_dwelling=Dwelling.objects.get(name="dwelling_0"))
 
 
 @receiver(post_save, sender=User)
